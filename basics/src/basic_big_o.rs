1bfee42a1f4f07da8c633469d36cbf9490025ba7
michael <michael.dewey131@gmail.com>
2023-03-29 00:18:13 -0700

    Gets the sum_char_codes function working

--- basics/src/basic_big_o.rs
+++ basics/src/basic_big_o.rs
@@ -1,15 +1,13 @@
 /// A simple function to define a sum based on a string of integers
 
-pub fn sum_char_codes(n: &str) -> u8 {
+pub fn sum_char_codes(text: &str) -> u32 {
     let mut sum = 0;
-    for i in 0..n.len() {
+    for i in text.chars() {
         // Remember, rust doesn't let you just slice a string due to UTF-8 encoding 
         // Instead, since we're sure that this is going to contain ASCII characters,
         // we use as_bytes()
-        let adder = n.as_bytes()[i];
-        println!("pre sum: {sum}");
+        let adder = i.to_digit(10).expect("Please only give my numbers");
         sum += adder;
-        println!("post sum: {sum}")
 
     }
     return sum;
