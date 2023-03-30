6b0ba38162fb795331821d5f7edbc1c1f4b5e672
michael <michael.dewey131@gmail.com>
2023-03-29 18:21:06 -0700

    Adds working bubble sort

--- sort/src/bubble_sort.rs
+++ sort/src/bubble_sort.rs
@@ -1,19 +1,31 @@
 /// Implements bubble sort with a mutable array reference
-pub fn bubble_sort(mut array: [u32]) -> [u32] {
+/// This has to be done with a mutable reference to the array,
+/// because rust needs a fixed size to allocate on the stack 
+pub fn bubble_sort(array: &mut [u32]) {
     // We need to iterate over each element of the array in the first loop except the first. -- i in 0..n - 1
     // In the second, we iterate over every element except for the last two    -- i in 0..n-2
     // In the third, we iterate over every element except for the last three -- i in 0..n-3
     // ...
     // In the n-1th, we iterate over every element except for the last n-1    -- i in 0..n-(n-1) = 0..1
-    // And we're done at n-2, because that loop is just 0
-    for i in 0..array.len() - 1 {
-        for j in 0..(array.len() - i) {
+    // And we're done at n-1, because that loop executes just once
+    for i in 0..array.len() {
+        for j in 0..(array.len() - i - 1) {
             if array[j] > array[j+1] {
-                let tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
+                array.swap(j, j+1);
             }
         }
     }
-    return array;
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sorts_array() {
+        let mut array = [7, 5, 6, 1, 8, 3];
+        bubble_sort(&mut array);
+        assert_eq!(array, [1, 3, 5, 6, 7, 8]);
+    }
 }
--- sort/src/main.rs
+++ sort/src/main.rs
@@ -5,6 +5,7 @@ use bubble_sort::bubble_sort;
 fn main() {
     let mut to_sort = [61, 3, 1, 2];
     println!("Unsorted: {:?}", to_sort);
-    to_sort = bubble_sort(to_sort);
+    bubble_sort(&mut to_sort);
     println!("Sorted: {:?}", to_sort);
+
 }
